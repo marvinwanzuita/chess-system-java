3b199ccaa64f8aa3c54edb83737b96abe4a8b715
Marvin <marvinwanzuita@gmail.com>
2022-06-28 18:33:19 -0300

    Starting to implement Board and Piece

---
+++ boardgame/Piece.java
@@ -0,0 +1,18 @@
+package JavaCompleto.ChessSystem.boardgame;
+
+public class Piece {
+
+  protected Position position;
+  private Board board;
+
+  public Piece(Board board){
+    this.board = board;
+    position = null;
+  }
+
+  protected Board getBoard(){
+    return board;
+  }
+
+
+}
