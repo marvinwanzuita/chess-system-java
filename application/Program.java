fe286d9edbf21822aff8e0155c2a70fb56490f45
Marvin <marvinwanzuita@gmail.com>
2022-06-29 13:54:39 -0300

    Moving pieces

--- application/Program.java
+++ application/Program.java
@@ -2,13 +2,33 @@ package JavaCompleto.ChessSystem.application;
 
 import JavaCompleto.ChessSystem.boardgame.Position;
 import JavaCompleto.ChessSystem.chess.ChessMatch;
+import JavaCompleto.ChessSystem.chess.ChessPiece;
+import JavaCompleto.ChessSystem.chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
   public static void main(String[] args) {
 
+    Scanner sc = new Scanner(System.in);
     ChessMatch chessMatch = new ChessMatch();
-    UI.printBoard(chessMatch.getPieces());
+
+
+    while (true){
+
+      System.out.println();
+      UI.printBoard(chessMatch.getPieces());
+
+      System.out.println();
+      System.out.print("Source: ");
+      ChessPosition source = UI.readChessPosition(sc);
+
+      System.out.print("Target: ");
+      ChessPosition target = UI.readChessPosition(sc);
+
+      ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+    }
 
 
   }
--- chess/ChessException.java
+++ chess/ChessException.java
@@ -1,6 +1,8 @@
 package JavaCompleto.ChessSystem.chess;
 
-public class ChessException extends RuntimeException {
+import JavaCompleto.ChessSystem.boardgame.BoardException;
+
+public class ChessException extends BoardException {
 
   public ChessException(String msg){
     super(msg);
